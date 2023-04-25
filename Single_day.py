869bd3279a2ac37ed611e9a7a7a357b6af26631e
Wojtek <wojtek.karcz@gmail.com>
2023-04-25 21:14:34 +0200

    first test with whole page corrected

--- Single_day.py
+++ Single_day.py
@@ -39,6 +39,94 @@ def date_range(date):
     start_date = (datetime.strptime(end_date,'%Y-%m-%d %H:%M:%S')-timedelta(days=1)).strftime('%Y-%m-%d')
     return start_date, end_date
 
+def get_bike_rides_metrics(df, date):
+    df_day = df.loc[df['start_time'].dt.date == pd.Timestamp(date)]
+    df_day_before = df.loc[df['start_time'].dt.date == pd.Timestamp(date-timedelta(days=1))]
+
+    total_rides = df_day['uid'].count()
+    total_rides_delta = total_rides - df_day_before['uid'].count()
+
+    avg_ride_duration = round(df_day.loc[df_day['duration'] > 1]['duration'].mean(),1)
+    avg_ride_duration_delta = round(avg_ride_duration - round(df_day_before.loc[df_day_before['duration'] > 1]['duration'].mean(),1),1)
+
+    avg_ride_length = round(df_day.loc[df_day['distance'] > 1]['distance'].mean(),2)
+    avg_ride_length_delta = round(avg_ride_length - round(df_day_before.loc[df_day_before['distance'] > 1]['distance'].mean(),2),2)
+
+    return total_rides, total_rides_delta, avg_ride_duration, avg_ride_duration_delta, avg_ride_length, avg_ride_length_delta
+
+def dist_plot_bike_rides(df, date):
+    data = df.loc[df['start_time'].dt.date == pd.Timestamp(date)].groupby(df['start_time'].dt.hour)['uid'].count()
+
+    return st.bar_chart(data)
+
+def create_df_bike_stations(df, date):
+    rental_df = df.loc[df['start_time'].dt.date == pd.Timestamp(date)].groupby('rental_place')['uid'].count().reset_index(name='rental_count')
+    return_df = df.loc[df['start_time'].dt.date == pd.Timestamp(date)].groupby('return_place')['uid'].count().reset_index(name='return_count')
+
+    temp = pd.merge(rental_df,return_df, left_on='rental_place', right_on='return_place', how='left')
+    temp = temp.drop(columns='return_place')
+    temp = temp.rename(columns={'rental_place':'bike_station'})
+    temp['return_count'] = temp['return_count'].fillna(0)
+    temp['diff'] = temp['return_count'] - temp['rental_count']
+    temp = temp[temp['bike_station'] != 'Poza stacją']
+    temp = temp.sort_values(ascending=False, by='rental_count')
+
+    return temp
+
+def revenue(row):
+    x = row['duration']
+    if x > 20 and x < 61:
+        return 2
+    elif x >= 61:
+        if x > 720:
+            return 2 + ((x // 60) * 4) + 300
+        else:
+            return 2 + ((x // 60) * 4)
+    else:
+        return 0
+
+def load_df_current_day(df, date): 
+    return df.loc[df['start_time'].dt.date == pd.Timestamp(date)]
+
+def create_df_misc_info(info):
+    info_stations_outside = info.loc[info['return_place'] == 'Poza stacją']['uid'].count()
+    info_stations_outside_ratio = round(info_stations_outside / info['uid'].count() * 100,1)
+    info_outside_bikes_fine = info_stations_outside * 5
+    info_total_distance = round(info['distance'].sum(),2)
+    info_loop = info.loc[(info['rental_place'] == info['return_place']) & (info['rental_place'] != 'Poza stacją') & (info['duration'] > 1)]['uid'].count()
+    
+    info['revenue'] = info.apply(lambda row: revenue(row), axis=1)
+    info_total_revenue = info['revenue'].sum()
+
+    info_df = {'Bikes returned out of bike stations':('{} ({} %)').format(info_stations_outside,info_stations_outside_ratio),
+            'Total fine for returning bike out of bike station': ('{} PLN').format(info_outside_bikes_fine),
+            'Total distance*': ('{} km').format(info_total_distance),
+            'Total loops (same rental/return station)': info_loop,
+            'Estimated total revenue**': ('{} PLN').format(info_total_revenue)
+            }
+    
+    return info_df
+
+def create_df_traffic_map(info):
+
+    test_geo2 = info.groupby(['rental_place',info['start_time'].dt.hour,'lat_start','lon_start'])['uid'].count().reset_index().rename(columns=({'uid':'count','start_time':'hour'}))
+    g1 = test_geo2.groupby(['hour','rental_place']).agg(count = ('count','sum')).reset_index()
+    g1[['lat_start','lon_start']] = test_geo2.groupby(['hour','rental_place'])['lat_start','lon_start'].first().reset_index()[['lat_start','lon_start']]
+
+    return g1
+
+def plot_traffic_map(df):
+    fig = px.scatter_mapbox(df, lat="lat_start", lon="lon_start", hover_name='rental_place', 
+                            size='count', animation_frame='hour', size_max=25, zoom=11, width=0, height=750
+                            )
+    fig.update_layout(mapbox_style="carto-positron")
+    return st.plotly_chart(fig)
+
+
+
+
+
+
 
 def main():
 
@@ -76,117 +164,63 @@ def main():
     # Data loading (current day and day before)
     df = load_data(start_date, end_date)
 
-
     st.markdown('### '+ title_date + ' | ' + title_weekday)
     st.markdown('###')
 
+    total_rides, total_rides_delta, avg_ride_duration, avg_ride_duration_delta, avg_ride_length, avg_ride_length_delta = get_bike_rides_metrics(df, st.session_state.day)
 
-# Creating dataframe with all data for current date
-data = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day)].groupby(df['start_time'].dt.hour)['uid'].count()
+    # Ride time distribution chart and essential stats
+    col1, col2 = st.columns([4,1],gap='medium')
 
+    with col1:
+        st.markdown('Number of bike rentals by hour')
+        dist_plot_bike_rides(df, st.session_state.day)
 
-# Defining variables for the ride time distribution chart and essential stats
-df_day = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day)]
-df_day_before = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day-timedelta(days=1))]
-
-total_rides = df_day['uid'].count()
-total_rides_delta = total_rides - df_day_before['uid'].count()
-
-avg_ride_duration = round(df_day.loc[df_day['duration'] > 1]['duration'].mean(),1)
-avg_ride_duration_delta = round(avg_ride_duration - round(df_day_before.loc[df_day_before['duration'] > 1]['duration'].mean(),1),1)
-
-avg_ride_length = round(df_day.loc[df_day['distance'] > 1]['distance'].mean(),2)
-avg_ride_length_delta = round(avg_ride_length - round(df_day_before.loc[df_day_before['distance'] > 1]['distance'].mean(),2),2)
+    with col2:
+        st.metric(label="Total rides", value=total_rides, delta=str(total_rides_delta))
+        st.metric(label="Avg. ride time", value='{} m'.format(avg_ride_duration), delta=str(avg_ride_duration_delta))
+        st.metric(label="Avg. ride distance", value='{} km'.format(avg_ride_length), delta=str(avg_ride_length_delta))
 
 
-# Ride time distribution chart and essential stats
-col1, col2 = st.columns([4,1],gap='medium')
+    # Table with top rental stations
+    temp = create_df_bike_stations(df, st.session_state.day)
 
-with col1:
-    st.markdown('Number of bike rentals by hour')
-    st.bar_chart(data)
-
-with col2:
-    st.metric(label="Total rides", value=total_rides, delta=str(total_rides_delta))
-    st.metric(label="Avg. ride time", value='{} m'.format(avg_ride_duration), delta=str(avg_ride_duration_delta))
-    st.metric(label="Avg. ride distance", value='{} km'.format(avg_ride_length), delta=str(avg_ride_length_delta))
+    st.markdown('#### 📊 Bike stations')
+    st.markdown('The table shows bike stations ranked in terms of the number of rentals throughout the day. You can click on any column title to sort the data relative to it.')
+    st.dataframe(temp, height=210, use_container_width=True)
 
 
-# Table with top rental stations
-rental_df = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day)].groupby('rental_place')['uid'].count().reset_index(name='rental_count')
-return_df = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day)].groupby('return_place')['uid'].count().reset_index(name='return_count')
+    # Table with misc info 
+    st.markdown('#####')
+    st.markdown('#### 🎈 Misc data')
+    st.markdown('A table containing additional information related to bike rentals for that day.')
 
-temp = pd.merge(rental_df,return_df, left_on='rental_place', right_on='return_place', how='left')
-
-temp = temp.drop(columns='return_place')
-temp = temp.rename(columns={'rental_place':'bike_station'})
-temp['return_count'] = temp['return_count'].fillna(0)
-temp['diff'] = temp['return_count'] - temp['rental_count']
-temp = temp[temp['bike_station'] != 'Poza stacją']
-
-st.markdown('#### 📊 Bike stations')
-st.markdown('The table shows bike stations ranked in terms of the number of rentals throughout the day. You can click on any column title to sort the data relative to it.')
-st.dataframe(temp.sort_values(ascending=False, by='rental_count'), height = 210, use_container_width=True)
+    info = load_df_current_day(df, st.session_state.day)
+    info_df = create_df_misc_info(info)
+    st.table(info_df)
 
 
-# Table with misc info 
-info = df.loc[df['start_time'].dt.date == pd.Timestamp(st.session_state.day)]
+    # Map with bike rental frequency throughout the day
+    st.markdown('#####')
+    st.markdown('#### 🗺️ Activity map')
+    st.markdown('Map showing the number of rentals at each bike station per hour. Click on the play button and see the traffic animation on all stations throughout the day. '+
+                'If you hover your mouse over the bubble, you will see the name of the station and the exact number of rentals during a specific hour of the day.')
 
-info_stations_outside = info.loc[info['return_place'] == 'Poza stacją']['uid'].count()
-info_stations_outside_ratio = round(info_stations_outside / info['uid'].count() * 100,1)
-info_outside_bikes_fine = info_stations_outside * 5
-info_total_distance = round(info['distance'].sum(),2)
-info_loop = info.loc[(info['rental_place'] == info['return_place']) & (info['rental_place'] != 'Poza stacją') & (info['duration'] > 1)]['uid'].count()
+    g1 = create_df_traffic_map(info)
+    plot_traffic_map(g1)
 
-def revenue(row):
-    x = row['duration']
-    if x > 20 and x < 61:
-        return 2
-    elif x >= 61:
-        if x > 720:
-            return 2 + ((x // 60) * 4) + 300
-        else:
-            return 2 + ((x // 60) * 4)
-    else:
-        return 0
-
-info['revenue'] = info.apply(lambda row: revenue(row), axis=1)
-info_total_revenue = info['revenue'].sum()
-
-info_df = {'Bikes returned out of bike stations':('{} ({} %)').format(info_stations_outside,info_stations_outside_ratio),
-           'Total fine for returning bike out of bike station': ('{} PLN').format(info_outside_bikes_fine),
-           'Total distance*': ('{} km').format(info_total_distance),
-           'Total loops (same rental/return station)': info_loop,
-           'Estimated total revenue**': ('{} PLN').format(info_total_revenue)
-           }
-st.markdown('#####')
-st.markdown('#### 🎈 Misc data')
-st.markdown('A table containing additional information related to bike rentals for that day.')
-
-st.table(info_df)
+ 
+    
+    
 
 
-# Map with bike rental frequency throughout the day
-test_geo2 = info.groupby(['rental_place',info['start_time'].dt.hour,'lat_start','lon_start'])['uid'].count().reset_index().rename(columns=({'uid':'count','start_time':'hour'}))
-g1 = test_geo2.groupby(['hour','rental_place']).agg(count = ('count','sum')).reset_index()
-g1[['lat_start','lon_start']] = test_geo2.groupby(['hour','rental_place'])['lat_start','lon_start'].first().reset_index()[['lat_start','lon_start']]
-
-fig2 = px.scatter_mapbox(g1, lat="lat_start", lon="lon_start", hover_name='rental_place', 
-                        size='count', animation_frame='hour', size_max=25, zoom=11, width=0, height=750
-                        )
-
-fig2.update_layout(mapbox_style="carto-positron")
-
-st.markdown('#####')
-st.markdown('#### 🗺️ Activity map')
-st.markdown('Map showing the number of rentals at each bike station per hour. Click on the play button and see the traffic animation on all stations throughout the day. '+
-            'If you hover your mouse over the bubble, you will see the name of the station and the exact number of rentals during a specific hour of the day.')
-
-st.write(fig2)
+    # Footnotes
+    st.markdown('#')
+    st.markdown('#')
+    st.info('''\* - *the total distance traveled by users during the day is calculated as the sum of the distances between stations in a straight line. Of course, we don\'t know what specific route users take, but this data gives general overview about the total length of rides that day.*  
+    \** - *the total revenue obtained on a given day for renting bikes is in accordance with the current Wrocław City Bike price list. However, it does not include the revenue from e-bikes because the available data does not include information about the type of bicycles.*  ''')
 
 
-# Footnotes
-st.markdown('#')
-st.markdown('#')
-st.info('''\* - *the total distance traveled by users during the day is calculated as the sum of the distances between stations in a straight line. Of course, we don\'t know what specific route users take, but this data gives general overview about the total length of rides that day.*  
-\** - *the total revenue obtained on a given day for renting bikes is in accordance with the current Wrocław City Bike price list. However, it does not include the revenue from e-bikes because the available data does not include information about the type of bicycles.*  ''')
+
+if __name__ == '__main__':
+    main()
